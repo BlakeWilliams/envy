03564e28e2f7cf95c1a17a9d5427174f64d981d8
Blake Williams <blake@blakewilliams.me>
2015-10-31 00:55:43 -0400

    Get ready for hex release
    * Setup package information for hex
    * Add ex_doc for documentation + hexdocs

--- mix.exs
+++ mix.exs
@@ -7,26 +7,25 @@ defmodule Envy.Mixfile do
      elixir: "~> 1.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps]
+     deps: deps,
+     package: package]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:earmark, "~> 0.1", only: :dev},
+     {:ex_doc, "~> 0.10", only: :dev}]
+  end
+
+  def package do
+    %{
+      description: "A package for managing env files",
+      licenses: ["MIT"],
+      maintainers: ["Blake Williams"],
+      links: %{github: "https://github.com/BlakeWilliams/envy"},
+    }
   end
 end
