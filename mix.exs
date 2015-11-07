b6b92c12471f7ba5a5f5d1b88daf6b191472297b
Blake Williams <blake@blakewilliams.me>
2015-11-07 12:11:37 -0500

    Bump version to 0.0.2

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Envy.Mixfile do
 
   def project do
     [app: :envy,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
