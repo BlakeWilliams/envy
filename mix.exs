5218e64d28739c7eeff37c9f1f5711026c92c0eb
Adrian Mugnolo <adrian@mugnolo.com>
2017-02-02 11:45:22 -0300

    Add parentheses to avoid ambiguity
    Fix warning messages on 1.4.

--- mix.exs
+++ mix.exs
@@ -7,8 +7,8 @@ defmodule Envy.Mixfile do
      elixir: "~> 1.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps,
-     package: package]
+     deps: deps(),
+     package: package()]
   end
 
   def application do
